2daa749d2cbc384d65e451332428250fe0422a78
Annika Palm <annika.palm@hotmail.com>
2021-03-28 18:28:27 +0200

    Update 03/28/21

---
+++ corpus/streamlit_app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import os
+import pickle
+
+st.title('Corpus Explorer')
+
+with open('corpus_raw_1', 'rb') as f:
+	data = pickle.load(f)
+
+#data.drop(['title', 'body', 'score', 'comms_num'], axis=1, inplace=True)
+st.text('This app explores a corpus of dream narratives scraped from Reddit.')
+
+st.subheader('Raw data')
+st_ms = st.multiselect("Columns", data.columns.tolist())
+st.dataframe(data)
+
+
+st.subheader('Post times by hour')
+hist_values = np.histogram(
+    data['timestamp'].dt.hour, bins=24, range=(0,24))[0]
+
+st.bar_chart(hist_values)
+
+df_temp = data.groupby(['text_length', 'comms_num']).mean()
+
+st.dataframe(data)
